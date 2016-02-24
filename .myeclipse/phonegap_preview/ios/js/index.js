b805ca47a1758c4dd2731bb16610bb391c84f254
Paulrm121 <prmerrington@gmail.com>
2016-02-24 15:06:54 +0200

    SC4

--- .myeclipse/phonegap_preview/ios/js/index.js
+++ .myeclipse/phonegap_preview/ios/js/index.js
@@ -55,10 +55,11 @@ ref.addEventListener('loadstart', function(event) { alert(event.url); });
 
 function geo()
 {
-    // onSuccess Callback
+   // onSuccess Callback
     // This method accepts a Position object, which contains the
     // current GPS coordinates
     //
+	
     var onSuccess = function(position) {
         alert('Latitude: '          + position.coords.latitude          + '\n' +
               'Longitude: '         + position.coords.longitude         + '\n' +
@@ -68,13 +69,14 @@ function geo()
               'Heading: '           + position.coords.heading           + '\n' +
               'Speed: '             + position.coords.speed             + '\n' +
               'Timestamp: '         + position.timestamp                + '\n');
+        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { enableHighAccuracy: true });
     };
 
     // onError Callback receives a PositionError object
     //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+function onError(error) {
+   alert('code: '    + error.code    + '\n' +
+       'message: ' + error.message + '\n');
     }
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError);	
