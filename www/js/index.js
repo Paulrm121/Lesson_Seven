3f3be1f4ba3302b7436fab4ad309e0d65c09b4a3
Paulrm121 <prmerrington@gmail.com>
2016-02-24 15:25:01 +0200

    Sc5

--- www/js/index.js
+++ www/js/index.js
@@ -55,21 +55,8 @@ ref.addEventListener('loadstart', function(event) { alert(event.url); });
 
 function geo()
 {
-   // onSuccess Callback
-    // This method accepts a Position object, which contains the
-    // current GPS coordinates
-    //
-	
-    var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { enableHighAccuracy: true });
+	var ref = window.open('https://whereamirightnow.com', '_blank', 'location=yes');
+	ref.addEventListener('loadstart', function(event) { alert(event.url); });
     };
 
     // onError Callback receives a PositionError object
